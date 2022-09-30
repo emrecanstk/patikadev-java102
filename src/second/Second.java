3e8e753933df6e472ba39ed2a623d7b46481e9c8
emrecanstk <emrecanstk@gmail.com>
2022-09-30 20:16:41 +0300

    Update README.md

--- src/second/Second.java
+++ src/second/Second.java
@@ -5,6 +5,6 @@ import first.First;
 public class Second {
     public static void main(String[] args) {
         First b = new First("hi");   // başka bir package'teki public sınıfımızdan nesne üretebildik.
-        System.out.println(b.str);       // başka bir package'teki sınıftan türettiğimiz nesneye doğrudan değer atadık.
+        System.out.println(b.str);       // başka bir package'teki sınıftan türettiğimiz nesnenin bilgisini doğrudan çağırdık.
     }
 }
