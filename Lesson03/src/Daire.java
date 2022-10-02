2f2ab2e05fd585ea3d9f479834a2020694e26ffe
emrecanstk <emrecanstk@gmail.com>
2022-10-02 21:33:43 +0300

    Add example for final

--- Lesson03/src/Daire.java
+++ Lesson03/src/Daire.java
@@ -1,4 +1,14 @@
 
 public class Daire {
-
+	public int r;
+	public final double pi = 3.14;   // final olarak işaretlenenler değiştirilemez
+	
+	public Daire(int r) {
+		this.r = r;
+	}
+	
+	public void calcArea() {
+		double area = pi * this.r * this.r;
+		System.out.println(area);
+	}
 }
