e6af9db38fd03b507e6c47428f252e0597da1ac1
emrecanstk <emrecanstk@gmail.com>
2022-10-02 20:43:00 +0300

    Add information about public-private-protected

--- src/first/Main.java
+++ src/first/Main.java
@@ -6,3 +6,9 @@ public class Main {
         System.out.println(a.str);    // public niteliğimize doğrudan eriştik. 
     }
 }
+
+
+// erişim belirleyici farkları:
+//    public:     aynı veya farklı tüm package'lerden erişilebilir.
+//    private:    aynı veya farklı hiçbir package'ten erişilemez.
+//    protected:  yalnızca aynı packagten erişilebilir.
