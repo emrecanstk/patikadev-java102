927df8bc2a23f15bef9b6c649ddd82b0df086a1f
emrecanstk <emrecanstk@gmail.com>
2022-10-02 21:18:26 +0300

    Add example for static

--- Lesson02/src/Main.java
+++ Lesson02/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		Player p1 = new Player("Emre",1);
 		System.out.println(p1.name);
-
+		
+		System.out.println(Player.game);          // static işaretlenen öğelerde nesne oluşturmak dahi gerekmez. Hafızaya işlenmiştir.
 	}
 
 }
--- Lesson02/src/Player.java
+++ Lesson02/src/Player.java
@@ -2,6 +2,7 @@
 public class Player {
     public String name;
     public int id;
+    public static String game = "AoE2";       // static ile işaretlendi.
     
     public Player(String name, int id) {
         this.name = name;
