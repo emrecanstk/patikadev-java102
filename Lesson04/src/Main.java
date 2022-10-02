d72645075f14c97af55f3f1fe5081be090c1a8e3
emrecanstk <emrecanstk@gmail.com>
2022-10-02 21:44:25 +0300

    Add example for encapsulation

--- Lesson04/src/Main.java
+++ Lesson04/src/Main.java
@@ -2,7 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		User user = new User("Emre",20,"53400000");
+		User user = new User();
+		
+		user.setName("Emre");
+		user.setAge(20);
+		user.setPhone("053400000");
 
 	}
 
--- Lesson04/src/User.java
+++ Lesson04/src/User.java
@@ -1,15 +1,34 @@
 
 public class User {
-	public String name;
-	public int age;
-	public String phone;
-	
-	public User(String name,int age,String phone) {
+	private String name;
+	private int age;
+	private String phone;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 	
-	
+	// değişkenlere doğrudan erişmek yerine getter setter kullandık
+	// buna encapsulation denir
 	
 }
