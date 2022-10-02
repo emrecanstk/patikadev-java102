bea3bd829422d6f141228cc3513c164ebf078191
emrecanstk <emrecanstk@gmail.com>
2022-10-02 21:41:46 +0300

    Add User.java

---
+++ Lesson04/src/User.java
@@ -0,0 +1,15 @@
+
+public class User {
+	public String name;
+	public int age;
+	public String phone;
+	
+	public User(String name,int age,String phone) {
+		this.name = name;
+		this.age = age;
+		this.phone = phone;
+	}
+	
+	
+	
+}
