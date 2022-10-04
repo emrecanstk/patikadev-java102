c1973adb1e539cd8af995dd60b1425c2a1e03453
emrecanstk <emrecanstk@gmail.com>
2022-10-04 20:37:24 +0300

    relations between classes

--- Lesson05/src/Main.java
+++ Lesson05/src/Main.java
@@ -6,7 +6,10 @@ public class Main {
 		Student st2 = new Student("Can", "2", "Ankara",94);
 		Student st3 = new Student("Ömer", "3", "İstanbul",31);
 		
+		Student[] students = {st1,st2,st3};
 		
+		Course c1 = new Course("Java", "102");
+		System.out.println(c1.calcAvarage(students));
 		
 	}
 
