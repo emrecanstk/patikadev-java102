7f4f3e43af0b7215474dc709fb4007dede00acb2
emrecanstk <emrecanstk@gmail.com>
2022-10-04 20:57:46 +0300

    inheritance

---
+++ Lesson06/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calisan c1 = new Calisan("Emre Can Satık", "53400000", "emrecanstk@gmail.com");
+		Akademisyen a1 = new Akademisyen("Ahmet Saygılı", "050000000", "asaygili@nku.edu.tr","Bilgisayar Mühendisliği","Prof.");
+		
+		c1.giris();
+		a1.giris();
+		a1.derseGir();
+	}
+
+}
