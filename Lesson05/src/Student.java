6ae0462c39613a3da6416384b7ff788eba923ddb
emrecanstk <emrecanstk@gmail.com>
2022-10-04 18:58:45 +0300

    Add Student.java

---
+++ Lesson05/src/Student.java
@@ -0,0 +1,39 @@
+
+public class Student {
+	private String name;
+	private String id;
+	private String adress;
+	
+	public Student(String name, String id, String adress) {
+		this.name = name;
+		this.id = id;
+		this.adress = adress;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+	
+	
+}
